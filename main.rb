9a8e0e5aac68b68f55f250b7f54283b9204c05a9
Alex Martín <alex.martin@returnly.com>
2021-12-25 23:49:56 +0100

    Add launcher

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'mqtt'
+require 'tesla_api'
+require_relative 'lib/mqttesla'
+
+mqtt_host = ENV['MQTT_HOST'] || '127.0.0.1'
+mqtt_port = ENV['MQTT_PORT'] || '1883'
+mqtt_ssl = ENV['MQTT_SSL'] == 'true'
+mqtt_topic = ENV['MQTT_TOPIC'] || 'mqttesla'
+max_api_retries = ENV['MAX_API_RETRIES'] || 3
+
+mqtt = MQTT::Client.connect(
+  host: mqtt_host,
+  port: mqtt_port,
+  ssl: mqtt_ssl
+)
+
+logger = Logger.new(STDOUT)
+
+logger.info { "Starting MQTTesla listening on #{mqtt_ssl ? 'mqtts' : 'mqtt'}://#{mqtt_host}:#{mqtt_port}/#{mqtt_topic}" }
+Mqttesla.new(
+  mqtt_client: mqtt,
+  logger: logger,
+  mqtt_topic: mqtt_topic
+).start(max_retries: max_api_retries)

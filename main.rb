49a058a3ab8daf40f18698442b326e8217db33bd
Alex Martín <alex.martin@returnly.com>
2021-12-26 15:49:04 +0100

    Log to STDERR by default (docker friendly)

--- main.rb
+++ main.rb
@@ -2,6 +2,11 @@ require 'mqtt'
 require 'tesla_api'
 require_relative 'lib/mqttesla'
 
+def log_device
+  ENV['LOG_DEVICE']&.upcase == 'STDOUT' ? STDOUT : STDERR
+end
+
+
 mqtt_host = ENV['MQTT_HOST'] || '127.0.0.1'
 mqtt_port = ENV['MQTT_PORT'] || '1883'
 mqtt_ssl = ENV['MQTT_SSL'] == 'true'
@@ -14,7 +19,7 @@ mqtt = MQTT::Client.connect(
   ssl: mqtt_ssl
 )
 
-logger = Logger.new(STDOUT)
+logger = Logger.new(log_device)
 
 logger.info { "Starting MQTTesla listening on #{mqtt_ssl ? 'mqtts' : 'mqtt'}://#{mqtt_host}:#{mqtt_port}/#{mqtt_topic}" }
 Mqttesla.new(
